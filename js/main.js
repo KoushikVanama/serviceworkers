89e80a401bcf4c506dfb5ae501048ca2ffec7a3f
sai koushik <saikoushik@vsvk.local>
2019-11-23 15:31:07 +0530

    changing to page caching

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      // .register('../sw_cached_pages.js')
-      .register('../sw_cached_site.js')
+      .register('../sw_cached_pages.js')
+      // .register('../sw_cached_site.js')
       .then(reg => {
         console.log('sw registered:', reg);
       })
